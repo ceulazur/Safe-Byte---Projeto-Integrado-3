a9605479a53eb2953cc9298995ddb5882225898c
hygor <hygorleal@gmail.com>
2024-12-10 02:38:56 -0300

    feat: config home screen navigation
    Signed-off-by: hygor <hygorleal@gmail.com>

--- app/src/main/java/com/example/safebyte/navigation/NavGraph.kt
+++ app/src/main/java/com/example/safebyte/navigation/NavGraph.kt
@@ -1,27 +1,31 @@
-package com.example.safebyte.navigation
+package com.example.safebyte.ui.navigation
 
+import HomeScreen
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.example.safebyte.ui.screens.HomeScreen
 import com.example.safebyte.ui.screens.LoginScreen
 
 @Composable
-fun AppNavGraph(
-    navController: NavHostController,
-    isLoggedIn: Boolean,
-    onLoginSuccess: () -> Unit
-) {
+fun NavGraph(navController: NavHostController) {
     NavHost(
         navController = navController,
-        startDestination = if (isLoggedIn) "home" else "login"
+        startDestination = "login" // Tela inicial
     ) {
         composable("login") {
-            LoginScreen(onLoginSuccess = onLoginSuccess)
+            LoginScreen(onLoginSuccess = {
+                // Navegar para a HomeScreen após login bem-sucedido
+                navController.navigate("home") {
+                    popUpTo("login") { inclusive = true } // Remove a tela de login da pilha de navegação
+                }
+            })
         }
+
         composable("home") {
-            HomeScreen()
+            HomeScreen(userName = "Francisco", onButtonClick = { label ->
+                println("Botão clicado: $label")
+            })
         }
     }
 }
