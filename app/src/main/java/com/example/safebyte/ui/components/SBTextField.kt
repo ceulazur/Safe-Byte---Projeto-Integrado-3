d248817adbf263525ea0721c0ce47da8717a3be0
gustavohenriquefs <gustavohenriquefs.dev@gmail.com>
2024-12-14 17:56:06 -0300

    feat: removendo imports nao usados  correcao do tema de cores e logo

--- app/src/main/java/com/example/safebyte/ui/components/SBTextField.kt
+++ app/src/main/java/com/example/safebyte/ui/components/SBTextField.kt
@@ -15,14 +15,12 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
