da6dba928b612b092f4ea425ccf6399c73f80f1a
gustavohenriquefs <gustavohenriquefs.dev@gmail.com>
2025-02-04 02:33:25 -0300

    refactor: corrigindo cores dos temas

--- app/src/main/java/com/example/safebyte/ui/theme/Theme.kt
+++ app/src/main/java/com/example/safebyte/ui/theme/Theme.kt
@@ -1,13 +1,12 @@
 package com.example.safebyte.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
+    primary = Blue50,
     secondary = PurpleGrey80,
     tertiary = Pink80,
     onSecondaryContainer = Blue800,
