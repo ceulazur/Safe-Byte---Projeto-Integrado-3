89705d3526f7636e99b1e4cd07579c39d4510d27
ceulazur <goncalvesjhordanna@gmail.com>
2025-02-14 18:59:22 -0300

    alergias

---
+++ app/backend/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from database import create_supabase_client
+from routes import allergysymptoms, doctor, productallergies, symptoms, user, allergies,  userallergies
+from routes import doctorknowallergies, doctorreview
+from routes import  establishment, establishmentallergies, establishmentreview
+from routes import  productreview, products
+from routes import tips
+app = FastAPI()
+
+# Initialize supabase client
+supabase = create_supabase_client()
+
+#users
+app.include_router(user.router)
+app.include_router(userallergies.router)
+#tips
+app.include_router(tips.router)
+#produtos
+app.include_router(products.router)
+app.include_router(productreview.router)
+app.include_router(productallergies.router)
+#alergias
+app.include_router(allergies.router)
+app.include_router(allergysymptoms.router)
+#estabelecimentos
+app.include_router(establishmentreview.router)
+app.include_router(establishmentallergies.router)
+app.include_router(establishment.router)
+#medicos
+app.include_router(doctorreview.router)
+app.include_router(doctorknowallergies.router)
+app.include_router(doctor.router)
+#simtomas
+app.include_router(symptoms.router)
+
