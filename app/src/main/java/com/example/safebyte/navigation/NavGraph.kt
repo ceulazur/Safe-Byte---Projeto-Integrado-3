b6a0cd9c377293d22af3eff96250c9dacf05d54c
hygorleal <hygorleal@gmail.com>
2025-02-16 18:02:32 -0300

    feat: add tela restaurante to NavGraph

--- app/src/main/java/com/example/safebyte/navigation/NavGraph.kt
+++ app/src/main/java/com/example/safebyte/navigation/NavGraph.kt
@@ -5,6 +5,7 @@ import Doctor
 import DoctorSearchScreen
 import HomeScreen
 import MyAllergiesScreen
+import android.net.Uri
 import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.ExitTransition
 import androidx.compose.animation.ExperimentalAnimationApi
@@ -16,6 +17,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
@@ -23,6 +25,9 @@ import com.example.safebyte.ui.screens.AllergyHistoryScreen
 import com.example.safebyte.ui.screens.DoctorProfileScreen
 import com.example.safebyte.ui.screens.FAQScreen
 import com.example.safebyte.ui.screens.LoginScreen
+import com.example.safebyte.ui.screens.Restaurant
+import com.example.safebyte.ui.screens.RestaurantScreen
+import com.example.safebyte.ui.screens.RestaurantSearchScreen
 import com.example.safebyte.ui.screens.SettingsScreen
 import com.example.safebyte.ui.screens.SignUpScreen
 import com.example.safebyte.ui.viewmodel.AuthViewModel
@@ -51,15 +56,12 @@ fun NavGraph(
         },
         exitTransition = {
             if (isAnimationsEnabled) {
-                fadeOut(
-                    animationSpec = tween(300)
-                )
+                fadeOut(animationSpec = tween(300))
             } else {
                 ExitTransition.None
             }
         }
     ) {
-        // Tela de login
         composable("login") {
             LoginScreen(
                 authViewModel = authViewModel,
@@ -67,13 +69,10 @@ fun NavGraph(
             )
         }
 
-        // Tela inicial (Home)
         composable(route = "home") {
             LaunchedEffect(Unit) {
                 settingsViewModel.loadThemeState(context)
-
                 settingsViewModel.loadNotificationState(context)
-
                 settingsViewModel.loadAnimationState(context)
             }
 
@@ -86,18 +85,17 @@ fun NavGraph(
                         "Minhas alergias" -> navController.navigate("my_allergies")
                         "Intolerância a lactose" -> navController.navigate("allergies_info")
                         "Médico" -> navController.navigate("doctor_search")
+                        "Restaurantes" -> navController.navigate("restaurant_search")
                         else -> println("Botão clicado: $label")
                     }
                 }
             )
         }
 
-        // Tela de histórico alérgico
         composable("allergy_history") {
             AllergyHistoryScreen(navController = navController)
         }
 
-        // Tela de Minhas Alergias
         composable("my_allergies") {
             MyAllergiesScreen(navController = navController)
         }
@@ -106,12 +104,10 @@ fun NavGraph(
             FAQScreen(onNavigateBack = { navController.popBackStack() })
         }
 
-        // Info Alergias
         composable("allergies_info") {
             AllergyInfoScreen(navController = navController)
         }
 
-        // Tela de sign up
         composable("sign_up") {
             SignUpScreen(
                 onSignUpSuccess = {
@@ -121,7 +117,6 @@ fun NavGraph(
             )
         }
 
-        //Tela de Settings
         composable("settings") {
             SettingsScreen(
                 viewModel = settingsViewModel,
@@ -131,34 +126,72 @@ fun NavGraph(
             )
         }
 
-
-        //Doctor Search
         composable("doctor_search") {
-            DoctorSearchScreen(navController = navController)
+            DoctorSearchScreen(
+                navController = navController,
+                onDoctorSelected = { doctor ->
+                    val encodedName = Uri.encode(doctor.name)
+                    val encodedLocation = Uri.encode(doctor.location)
+                    val doctorRating = doctor.rating.toString()
+
+                    navController.navigate("doctor_profile/$encodedName/$encodedLocation/$doctorRating")
+                }
+            )
         }
 
-        // Doctor Profile
         composable(
             "doctor_profile/{doctor_name}/{doctor_location}/{doctor_rating}",
             arguments = listOf(
-                navArgument("doctor_name") { defaultValue = "" },
-                navArgument("doctor_location") { defaultValue = "" },
-                navArgument("doctor_rating") { defaultValue = 0.0 }
+                navArgument("doctor_name") { type = NavType.StringType },
+                navArgument("doctor_location") { type = NavType.StringType },
+                navArgument("doctor_rating") { type = NavType.FloatType }
             )
         ) { backStackEntry ->
             val doctorName = backStackEntry.arguments?.getString("doctor_name") ?: ""
             val doctorLocation = backStackEntry.arguments?.getString("doctor_location") ?: ""
-            val doctorRating =
-                backStackEntry.arguments?.getString("doctor_rating")?.toDoubleOrNull() ?: 0.0
+            val doctorRating = backStackEntry.arguments?.getFloat("doctor_rating") ?: 0.0f
 
             val doctor = Doctor(
                 name = doctorName,
                 location = doctorLocation,
-                rating = doctorRating
+                rating = doctorRating.toDouble()
             )
-            DoctorProfileScreen(doctor = doctor)
 
+            DoctorProfileScreen(doctor = doctor)
+        }
+
+        composable("restaurant_search") {
+            RestaurantSearchScreen(
+                navController = navController,
+                onRestaurantSelected = { restaurant ->
+                    val encodedName = Uri.encode(restaurant.name)
+                    val encodedLocation = Uri.encode(restaurant.location)
+                    val restaurantRating = restaurant.rating.toString()
+
+                    navController.navigate("restaurant/${encodedName}/${encodedLocation}/${restaurantRating}")
+                }
+            )
+        }
+
+        composable(
+            "restaurant/{restaurant_name}/{restaurant_location}/{restaurant_rating}",
+            arguments = listOf(
+                navArgument("restaurant_name") { type = NavType.StringType },
+                navArgument("restaurant_location") { type = NavType.StringType },
+                navArgument("restaurant_rating") { type = NavType.FloatType }
+            )
+        ) { backStackEntry ->
+            val restaurantName = backStackEntry.arguments?.getString("restaurant_name") ?: ""
+            val restaurantLocation = backStackEntry.arguments?.getString("restaurant_location") ?: ""
+            val restaurantRating = backStackEntry.arguments?.getFloat("restaurant_rating") ?: 0.0f
+
+            val restaurant = Restaurant(
+                name = restaurantName,
+                location = restaurantLocation,
+                rating = restaurantRating.toDouble()
+            )
+
+            RestaurantScreen(restaurant = restaurant)
         }
     }
 }
-
